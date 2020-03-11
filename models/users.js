72e856b67e080ab03f2108d7049e0789d77d910d
Amir Gorji <dc_master95@gmail.com>
2020-03-11 15:23:09 +0330

    Hashing password

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 const { mongoose } = require('./mongoose');
 const joi = require('joi')
+const bcrypt = require('bcrypt');
+const _ = require('lodash');
 
 const UserSchema = new mongoose.Schema({
     firstName: {
@@ -28,6 +30,50 @@ const UserSchema = new mongoose.Schema({
         minlength: 8,
         maxlength: 1024,
         required: true
+    }
+});
+
+UserSchema.methods.toJSON = function() {
+    let user = this;
+    let userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'firstName', 'firstName', 'username', 'email']);
+}
+
+UserSchema.statics.userLogin = function(username, password) {
+    let User = this;
+
+    return User.findOne({
+        username
+    }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+}
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
     }
 });
 
