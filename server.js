cfe7dc9a6fa348a6783e9e9fa79a933694411097
Amir Gorji <dc_master95@gmail.com>
2020-03-11 14:56:20 +0330

    Using Lodash

--- server.js
+++ server.js
@@ -2,19 +2,21 @@ const express = require('express');
 const { mongoose } = require('./models/mongoose');
 const { User, validateUser } = require('./models/users');
 const { createUser } = require('./functions/models');
-
+const _ = require('lodash');
 const app = express();
 app.use(express.json());
 
 app.post('/user/new', async(req, res) => {
     try {
-        let { error } = validateUser(req.body);
-        console.log(req.body);
+        const body = _.pick(req.body, ['firstName', 'lastName', 'username', 'password', 'email']);
+        let { error } = validateUser(body);
+        console.log(body);
         if (error) return res.status(400).send(error.details[0].message);
-        let user = await User.findOne({ email: req.body.email });
+        let user = await User.findOne({ email: body.email });
         if (user) return res.status(400).send('User already registered.');
 
-        user = await createUser(req.body);
+        user = new User(body);
+        await user.save();
         res.send({ user });
     } catch (error) {
         res.status(400).send({ error });
